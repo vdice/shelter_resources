0ae94b2c7c4540249c8bfce2698d72c79360b86c
Guest User <student@epicodus.com>
2015-09-02 15:50:04 -0700

    add item creation to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,30 @@
-Resource.create({:name => 'bed'})
-Resource.create({:name => 'meal'})
+resource_list = {
+  'hygiene products' => ['toothbrush', 'hairbrush'],
+  'linens' => ['blankets', 'towels'],
+  'clothing' => ['pants', 'sweaters'],
+  'media' => ['books', 'dvds'],
+  'toys' => ['board games', 'squirt guns'],
+  'food' => ['pizza', 'pasta']
+}
 
-Shelter.create({:name => 'Transition Projects', :address => '665 Northwest Hoyt Street, Portland, OR 97209', :phone_number => "321-456-7890"})
-Shelter.create({:name => 'Portland Rescue Mission', :address => '111 West Burnside Street, Portland, OR 97209', :phone_number => "321-456-7891"})
+resource_list.each() do |key, value|
+  Resource.create({:name => key})
+end
 
 shelter_list = [
-  ["City Team Ministries", "Portland, OR 97214", "503-231-9334"],
-  ["Portland Rescue Mission", "Portland, OR 97209", "503-906-7690"],
-  ["VETS (Veterans Emergency Transitional Shelter)", "Portland, OR 97204", "503-239-1259"],
-  ["Transition Projects TPI", "Portland, OR 97209", "503-823-4930"],
-  ["Salvation Army Female Emergency Shelter (SAFES)", "Portland, OR 97209", "503-227-0810"]
+  ["City Team Ministries", "526 SE Grand Ave,Portland, OR 97214", "503-231-9334"],
+  ["Portland Rescue Mission", "111 W Burnside St, Portland, OR 97209", "503-906-7690"],
+  ["VETS (Veterans Emergency Transitional Shelter)", "30 Sw 2nd Ave., Portland, OR 97204", "503-239-1259"],
+  ["Transition Projects TPI", "665 NW Hoyt St, Portland, OR 97209", "503-823-4930"],
+  ["Salvation Army Female Emergency Shelter (SAFES)", "30 SW 2nd Ave, Portland, OR 97204", "503-227-0810"]
 ]
 
-shelter_list.each do |name, address, phone_number|
-  Shelter.create(name: name, address: address, phone_number: phone_number)
+shelter_list.each() do |name, address, phone_number|
+  new_shelter = Shelter.create({:name => name, :address => address, :phone_number => phone_number})
+
+  Resource.all().each() do |resource|
+    resource_list.fetch(resource.name()).each() do |item_name|
+      Item.create({:name => item_name, :quantity => 0, :resource_id => resource.id(), :shelter_id => new_shelter.id()})
+    end
+  end
 end
