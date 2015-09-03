d5e6d9dbdd8f16da375eea01ca27737b0ec9e4b9
Travis Johnston <tjohnst1@gmail.com>
2015-09-02 21:21:32 -0700

    add id tags to the inputs in the donate view to enable autocomplete

---
+++ public/js/autocomplete.js
@@ -0,0 +1,7 @@
+$( "#locationComplete" ).autocomplete({
+  source: [ "c++", "java", "php", "coldfusion", "javascript", "asp", "ruby" ]
+});
+
+$( "#shelterComplete" ).autocomplete({
+  source: [ "c++", "java", "php", "coldfusion", "javascript", "asp", "ruby" ]
+});
