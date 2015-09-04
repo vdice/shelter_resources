ef2cf82056388006cf9bbf1e6b4a92208b754c20
Vaughn Dice <donvice@gmail.com>
2015-09-03 21:10:22 -0700

    convert resource buttons/links to icons

--- TODO.md
+++ TODO.md
@@ -1,8 +1,3 @@
-* change name from 'Homeless.io' to 'Shelter.io' or something more generic?
-   * for example, non-homeless woman seeking safe haven, etc.
-
-
-* Make manual entering of location optional, always try to autolocate
 
 * error handling w/ GoogleGeocoder3
   * empty result
--- app.rb
+++ app.rb
@@ -11,94 +11,30 @@ configure do
   set :geocoder, Geokit::Geocoders::GoogleGeocoder3
 end
 
-get('/locator') do
-  @resources = Resource.all()
-  erb(:locator)
-end
-
-post('/locator/results') do
-  shelters = Shelter.all()
-  @resources = Resource.all()
-
-  source = nil
-  # fetch user lat/lng auto-populated by map.js on window load
-  user_latitude = params.fetch('user_latitude')
-  user_longitude = params.fetch('user_longitude')
-
-  if (params.fetch('source_street').empty?)
-    source = [user_latitude,
-              user_longitude].join(', ')
-
-  else
-    source = [params.fetch('source_street'),
-              params.fetch('source_city'),
-              params.fetch('source_state')].join(', ')
-  end
-
-  @geocoded_source = settings.geocoder.geocode(source)
-  if @geocoded_source.full_address.empty?
-    @geocoded_source = settings.geocoder.geocode('Portland, OR')
-  end
-
-  # get resource selection and filter shelters based on
-  # which shelters have items in need
-  resource_id = params.fetch('resource_select').to_i()
-  found_shelters = []
-  shelters.each() do |shelter|
-    include_shelter = false
-    shelter.items.each() do |item|
-      if (item.resource_id().eql?(resource_id))
-        include_shelter = true if item.quantity().>(0)
-      end
-    end
-    found_shelters.push(shelter) if include_shelter
-  end
-
-  # calculate distance from source to each shelter
-  with_distance = []
-  found_shelters.each do |shelter|
-    geocoded_shelter = settings.geocoder.geocode(shelter.address())
-    distance = (geocoded_shelter).distance_to(@geocoded_source).round(2)
-    shelter.latitude=geocoded_shelter.lat
-    shelter.longitude=geocoded_shelter.lng
-    with_distance << [shelter, distance]
-  end
-
-  # sort according to distance (index 1 on with_distance array)
-  @sorted_shelters = with_distance.sort{|a, b| a[1] <=> b[1]}[0...5]
-
-  erb(:locator_results)
-end
-
 get('/') do
   erb :index
 end
 
 get '/donate' do
-  @resources = Resource.all.sort{ |x,y| x.name <=> y.name }
-  @shelter_arr = Shelter.all.map{|shelter_obj| shelter_obj.name }
-
-  @route_heading = 'Donate By:'
-  @action = 'donate'
-  @find_shelter_string = 'Find Shelters by What They Need:'
-  erb :find_by
+  get_find_by_page('donate')
 end
 
+get '/support' do
+  get_find_by_page('support')
+end
+
+post '/support' do
+  resolve_find_query()
+end
+
+post '/donate' do
+  resolve_find_query()
+end
 
 get '/resources/:id' do
   @resource = Resource.find(params['id'].to_i)
+  @shelters = Shelter.all()
   erb :resource_inventory
-end
-
-
-get '/support' do
-  @resources = Resource.all.sort{ |x,y| x.name <=> y.name }
-  @shelter_arr = Shelter.all.map{|shelter_obj| shelter_obj.name }
-
-  @route_heading = 'Find Support By:'
-  @action = 'support'
-  @find_shelter_string = 'Find Shelters by Resource:'
-  erb :find_by
 end
 
 get '/shelters/:id' do
@@ -106,34 +42,60 @@ get '/shelters/:id' do
   erb :shelter
 end
 
-post '/support' do
-  shelters = Shelter.all()
-
-  location = params.fetch('location')
-
-  if (location.empty?)
-    # TODO: auto locate user
-  else
-    @geocoded_source = settings.geocoder.geocode(location)
-
-    # calculate distance from source to each shelter
-    with_distance = []
-    shelters.each do |shelter|
-      geocoded_shelter = settings.geocoder.geocode(shelter.address())
-      distance = (geocoded_shelter).distance_to(@geocoded_source).round(2)
-      shelter.latitude=geocoded_shelter.lat
-      shelter.longitude=geocoded_shelter.lng
-      with_distance << [shelter, distance]
-    end
-
-    # sort according to distance (index 1 on with_distance array)
-    @sorted_shelters = with_distance.sort{|a, b| a[1] <=> b[1]}[0...5]
-
-  end
-  erb :locator_results
+# Helper Methods
+define_method(:capitalize) do |str|
+  HelperMethod.capitalize_multiple_words(str)
 end
 
-post '/support/shelters' do
-  @shelter = Shelter.find_by_name(params.fetch('shelter'))
-  redirect("/shelters/#{@shelter.id()}")
+define_method(:resolve_find_query) do
+  if (params.fetch('shelter').empty?)
+    @geocoded_source = get_geocoded_source()
+    @shelters = get_shelters_and_distances_from(@geocoded_source)
+    erb :results
+  else
+    @shelter = Shelter.find_by_name(params.fetch('shelter'))
+    redirect("/shelters/#{@shelter.id()}")
+  end
+end
+
+define_method(:get_find_by_page) do |type|
+  @resources = Resource.all.sort{ |x,y| x.name <=> y.name }
+  @shelter_arr = Shelter.all.map{|shelter_obj| shelter_obj.name }
+
+  support = type.eql?('support')
+  @route_heading = support ? 'Find Support:' : 'Donate:'
+  @action = support ? 'support' : 'donate'
+  @find_shelter_string = support ? 'Find Shelters by Resource:' : 'Find Shelters by What They Need:'
+
+  erb :find_by
+end
+
+define_method(:get_geocoded_source) do
+  # fetch user lat/lng auto-populated by map.js on window load
+  user_latitude = params.fetch('user_latitude')
+  user_longitude = params.fetch('user_longitude')
+  auto_populated_source = [user_latitude, user_longitude].join(', ')
+
+  source = params.fetch('location').empty? ? auto_populated_source :
+                                             params.fetch('location')
+
+  geocoded_source = settings.geocoder.geocode(source)
+  if geocoded_source.full_address.empty?
+    geocoded_source = settings.geocoder.geocode('Portland, OR')
+  end
+  geocoded_source
+end
+
+define_method(:get_shelters_and_distances_from) do |geocoded_source|
+  shelters = Shelter.all()
+
+  shelters_and_distances = []
+  shelters.each do |shelter|
+    geocoded_shelter = settings.geocoder.geocode(shelter.address())
+    distance = (geocoded_shelter).distance_to(geocoded_source).round(2)
+    shelter.latitude=geocoded_shelter.lat
+    shelter.longitude=geocoded_shelter.lng
+    shelters_and_distances << [shelter, distance]
+  end
+  shelters_and_distances.sort{|a, b| a[1] <=> b[1]}[0...5]
 end
