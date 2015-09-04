712b4c0691e23b0699abd89acfe46e4c26d0f916
Travis Johnston <tjohnst1@gmail.com>
2015-09-03 22:07:48 -0700

    refactor nav bar in layout.erb

--- app.rb
+++ app.rb
@@ -77,6 +77,7 @@ end
 get '/donate' do
   @resources = Resource.all.sort{ |x,y| x.name <=> y.name }
   @shelter_arr = Shelter.all.map{|shelter_obj| shelter_obj.name }
+  @route_heading = 'Donate:'
 
   @action = 'donate'
   erb :find_by
