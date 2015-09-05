f4e193ef4a1f31123ed4d45e2c64ec2657f54b31
Vaughn Dice <donvice@gmail.com>
2015-09-05 11:21:16 -0700

    DRY out index_pages_spec

--- spec/integration/index_pages_spec.rb
+++ spec/integration/index_pages_spec.rb
@@ -3,18 +3,18 @@ require('spec_helper')
 describe('the index/nav bar paths', {:type => :feature}) do
   before(:each) do
     @url = '/'
-    @donate = {:name => 'donate', :title => 'Donate:'}
-    @support = {:name => 'support', :title => 'Find Support:'}
-    @about_us = {:name => 'about_us', :title => 'About Us'}
+    @donate = {:name => 'donate', :title => 'Donate', :expected_content => 'Donate:'}
+    @support = {:name => 'support', :title => 'Find Support', :expected_content => 'Find Support:'}
+    @about_us = {:name => 'about_us', :title => 'About Us', :expected_content => 'About Us'}
   end
 
   describe('initial state') do
     it('greets user with initial state') do
       visit(@url)
       expect(page).to have_content('Xenia')
-      expect(page).to have_content('Donate')
-      expect(page).to have_content('Find Support')
-      expect(page).to have_content('About Us')
+      expect(page).to have_content(@donate[:title])
+      expect(page).to have_content(@support[:title])
+      expect(page).to have_content(@about_us[:title])
     end
   end
 
@@ -22,7 +22,7 @@ describe('the index/nav bar paths', {:type => :feature}) do
     it('takes user to the donate page') do
       visit(@url)
       find("##{@donate[:name]}_link").click
-      expect(page).to have_content(@donate[:title])
+      expect(page).to have_content(@donate[:expected_content])
     end
   end
 
@@ -30,7 +30,7 @@ describe('the index/nav bar paths', {:type => :feature}) do
     it('takes user to the support page') do
       visit(@url)
       find("##{@support[:name]}_link").click
-      expect(page).to have_content(@support[:title])
+      expect(page).to have_content(@support[:expected_content])
     end
   end
 
@@ -38,17 +38,17 @@ describe('the index/nav bar paths', {:type => :feature}) do
     it('takes user to donate page') do
       visit(@url)
       find("#nav_#{@donate[:name]}_link").click
-      expect(page).to have_content(@donate[:title])
+      expect(page).to have_content(@donate[:expected_content])
     end
     it('takes user to support page') do
       visit(@url)
       find("#nav_#{@support[:name]}_link").click
-      expect(page).to have_content(@support[:title])
+      expect(page).to have_content(@support[:expected_content])
     end
     it('takes the user to the about us section') do
       visit(@url)
       find("#nav_#{@about_us[:name]}_link").click
-      expect(page).to have_content(@about_us[:title])
+      expect(page).to have_content(@about_us[:expected_content])
     end
   end
 
@@ -56,17 +56,17 @@ describe('the index/nav bar paths', {:type => :feature}) do
     it('takes user to donate page') do
       visit(@url)
       find("#nav_mobile_#{@donate[:name]}_link").click
-      expect(page).to have_content(@donate[:title])
+      expect(page).to have_content(@donate[:expected_content])
     end
     it('takes user to support page') do
       visit(@url)
       find("#nav_mobile_#{@support[:name]}_link").click
-      expect(page).to have_content(@support[:title])
+      expect(page).to have_content(@support[:expected_content])
     end
     it('takes the user to the about us section') do
       visit(@url)
       find("#nav_mobile_#{@about_us[:name]}_link").click
-      expect(page).to have_content(@about_us[:title])
+      expect(page).to have_content(@about_us[:expected_content])
     end
   end
 
